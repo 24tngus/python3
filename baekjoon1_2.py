f3e9947e56a55480337a92054d704cf13b2fc97c
수현 <68477988+24tngus@users.noreply.github.com>
2022-10-26 22:23:12 +0900

    백준1

---
+++ baekjoon1_2.py
@@ -0,0 +1,8 @@
+#Q. 같은 아이디 있으면 ??! 출력
+
+id = ['joonas','baekjoon']
+
+new_id = list(input('새 아이디 입력'))
+
+if(id==new_id):
+    print(new_id+'??!')
---
+++ baekjoon1_3.py
@@ -0,0 +1,8 @@
+#Q. 체스 개수 맞추기
+
+chess = [1,1,2,2,2,8]
+
+number = list(map(int, input().split()))
+
+for i in range(len(chess)):
+    print(chess[i]-number[i],end='')
