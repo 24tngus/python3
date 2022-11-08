036a0c36dc73f90c04c2b713e8a150c8395ca016
수현 <68477988+24tngus@users.noreply.github.com>
2022-11-08 12:47:48 +0900

    Update 4_1 배열에서 숫자찾기.py

--- Baekjoon/4_1 배열에서 숫자찾기.py
+++ Baekjoon/4_1 배열에서 숫자찾기.py
@@ -1,7 +1,7 @@
 #Q. 배열에서 숫자 찾기 
 
 N = int(input()) # 정수의 개수 
-arr = list(map(int, input().split())) # 배열
+arr = list(map(int, input().split()[:N])) # 배열
 sel = int(input()) # 찾을 정수 
 
 # count함수 사용
