3a294564855fe1059f4189ab2f5c1f56fd1d5a05
수현 <68477988+24tngus@users.noreply.github.com>
2022-11-12 08:52:23 +0900

    Add files via upload

--- Baekjoon/bj4_4.py
+++ Baekjoon/bj4_4.py
@@ -1,8 +1,10 @@
-#Q. 배열에서 숫자 찾기 
+def solution(array, height):
+    answer = 0
+    
+    for i in array:
+       if i > height:
+        answer += 1
 
-N = int(input()) # 정수의 개수 
-arr = list(map(int, input().split()[:N])) # 배열
-sel = int(input()) # 찾을 정수 
-
-# count함수 사용
-print(arr.count(sel))
+        print(i)
+        
+    return answer
