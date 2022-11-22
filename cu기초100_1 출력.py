14971a0449fa0d4fc6e89eac48110c7d9ebfcd39
수현 <68477988+24tngus@users.noreply.github.com>
2022-11-22 23:45:40 +0900

    Add files via upload

---
+++ cu기초100_1 출력.py
@@ -0,0 +1,23 @@
+#Q. 1 문자열 출력(1)
+print("Hello")
+
+#Q. 2 문자열 출력 (2)
+print("Hello World")
+
+#Q. 3 특수 기호 출력 (1)
+print("Hello\nWorld")
+
+#Q. 4 특수 기호 출력 (2)
+print("\'Hello\'")
+
+#Q. 5 특수 기호 출력 (3)
+print("\"Hello World\"")
+
+#Q. 6 특수 기호 출력 (4)
+print("\"!@#$%^&*()\'")
+
+#Q. 7 특수 기호 출력 (5)
+print("\"C:\\Download\\\'hello\'.py\"")
+
+#Q. 8 특수 기호 출력 (6)
+print("print(\"Hello\\nWorld\")")
