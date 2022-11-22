bc3f9b89f836378b58f822a06f0f3410fb0dede4
수현 <68477988+24tngus@users.noreply.github.com>
2022-11-22 22:04:09 +0900

    Update 3_3 빠른 입출력.py

--- Baekjoon/단계별로 풀어보기/3_3 빠른 입출력.py
+++ Baekjoon/단계별로 풀어보기/3_3 빠른 입출력.py
@@ -4,12 +4,12 @@ import sys
 
 N = int(input())
 
-# 리스트 사용하지 않을 경우 
+# (1))
 for i in range(N):
     x, y = map(int, sys.stdin.readline().split())
     print(x+y)
 
-# 리스트 사용할 경우 
+# (2))
 a = [map(int, sys.stdin.readline().split()) for i in range(N)]
 
 for x, y in a:
