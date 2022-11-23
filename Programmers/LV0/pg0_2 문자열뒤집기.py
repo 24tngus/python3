7e26c6de3e3dcdd634f0eb266e616c1389876967
수현 <68477988+24tngus@users.noreply.github.com>
2022-11-23 15:07:18 +0900

    Update pg0_2 문자열뒤집기.py

--- Programmers/LV0/pg0_2 문자열뒤집기.py
+++ Programmers/LV0/pg0_2 문자열뒤집기.py
@@ -1,10 +1,15 @@
+#Q. 문자열 뒤집기 
+
+# 문자열 my_string이 매개변수로 주어집니다. 
+# my_string을 거꾸로 뒤집은 문자열을 return하도록 solution 함수를 완성해주세요.
+
 def solution(my_string):
     answer = ''
     
     # 1) 슬라이싱 이용 
     answer = [] # 문자열인 경우 answer = ''
     
-    answer = num_list[::-1] # 원소 순서를 거꾸로 뒤집기
+    answer = my_string[::-1] # 원소 순서를 거꾸로 뒤집기
     
     # 2) for문 이용
     for c in my_string:
