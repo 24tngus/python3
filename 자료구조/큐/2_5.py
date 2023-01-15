599b2faaf85c8342809939d794acf384cfe3842a
수현 <68477988+24tngus@users.noreply.github.com>
2023-01-16 01:20:11 +0900

    Update and rename 2222.py to 2_5.py

---
+++ 자료구조/큐/2_5.py
@@ -0,0 +1,15 @@
+
+---------------------------------------------------------------------------------------------------
+T = int(input())
+
+num = {"ZRO":0, "ONE":1, "TWO":2, "THR":3, "FOR":4, "FIV":5, "SIX":6, "SVN":7, "EGT":8, "NIN":9}
+for test_case in range(1, T+1):
+    n = input()
+    answer = 0
+
+    word = map(str, input().split())
+
+    for w in word:
+        print(num[w])
+    
+    print("#{} {}".format(test_case, answer))
